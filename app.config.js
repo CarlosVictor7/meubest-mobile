b54faff0ef4adba2a2bacee89750d1d488911777
CarlosVictor7 <carlos07victor@gmail.com>
2026-07-04 17:03:59 -0300

    chore: bump iOS build number to 1.1.1

--- app.config.js
+++ app.config.js
@@ -17,7 +17,7 @@ module.exports = {
   ios: {
     supportsTablet: false,
     bundleIdentifier: IS_DEV ? 'meu.best.dev' : 'meu.best',
-    buildNumber: '1.1.0',
+    buildNumber: '1.1.1',
     infoPlist: {
       NSCameraUsageDescription: 'Meu Best usa sua câmera para sessões de apoio em vídeo.',
       NSMicrophoneUsageDescription: 'Meu Best usa seu microfone para sessões de apoio em áudio/vídeo.',
